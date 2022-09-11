afd50b0dd5b956ab5540435b4d9975d313cf1dbb
YiJohnZhang <yijohnzhang@gmail.com>
2022-09-11 10:21:08 -0700

    `script.js` bug: can't pass `this.deckID`
    `script.js` bug: can't pass `this.deckID`. maybe refactor it to just stand-alone methods?

--- 03_31.03.10_async_&_await/script.js
+++ 03_31.03.10_async_&_await/script.js
@@ -1,17 +1,21 @@
 // 01. Number
 // http://numbersapi.com/42
 
-const BASEURL_01 = 'http://numbersapi.com/42';
+const fetchFavoriteNumber = async () => {
 
-fetchFavoriteNumber = () => {
+    const BASEURL_01 = 'http://numbersapi.com/42';
 
-    axios.get(BASEURL_01)
-        .then((response) => {
-            document.getElementById('01_numberFact').innerText = response.data;
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    try {
+
+        const response = await axios.get(BASEURL_01);
+        document.getElementById('01_numberFact').innerText = response.data;
+    
+    }catch(error){
+        console.error(error);
+    }
+
+
+    
 
 }
 
@@ -19,53 +23,43 @@ fetchFavoriteNumber = () => {
 // https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
 // https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=1
 
-const BASEURL_02_INITIALIZE_DECK = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
-let BASEURL_02_GET_DECK;
+const assignment02 = {
 
-displayDrawInformation = (cardDrawn, remainingCards) => {
+    async init(){
+        // (Re-)Initialize the Deck
 
-    document.getElementById('02_cardsLeft').innerText = `${remainingCards} Cards Remaining`;
+        const INITIALIZE_DECK_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+        const initialDeckResponse = await axios.get(INITIALIZE_DECK_URL);
+        this.deckID = initialDeckResponse.data.deck_id;
+        console.log(this.deckID);
 
-    // const CARD_IMAGE
-    const CARD_IMAGE_ELEMENT = document.createElement('img');
-    CARD_IMAGE_ELEMENT.src = cardDrawn.image;
+    },
 
-    const CARD_IMAGE_CONTAINER = document.getElementById('02_cardGame');
-    CARD_IMAGE_CONTAINER.prepend(CARD_IMAGE_ELEMENT);
+    async drawNextCard(){
 
-}
+        console.log(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=1`)
 
-drawNextCard = () => {
-
-    axios.get(BASEURL_02_GET_DECK)
-        .then((response) => {
-
-            // console.log(response.data.deck_id);
-            // console.log(response.data.remaining);
-            // console.log(response.data.cards);
-
-            displayDrawInformation(cardDrawn = response.data.cards[0], remainingCards = response.data.remaining);
-
-        })
-        .catch((error) => {
+        try{
+            const drawDeckResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=1`);
+            this.displayDrawInformation(cardDrawn = drawDeckResponse.data.cards[0], cardsRemaining = drawDeckResponse.data.remaining);
+        }catch(error){
             console.log(error);
-        });
+        }
+
+    }, 
+
+    displayDrawInformation(){
+
+        document.getElementById('02_cardsLeft').innerText = `${remainingCards} Cards Remaining`;
+
+        // const CARD_IMAGE
+        const CARD_IMAGE_ELEMENT = document.createElement('img');
+        CARD_IMAGE_ELEMENT.src = cardDrawn.image;
     
-}
+        const CARD_IMAGE_CONTAINER = document.getElementById('02_cardGame');
+        CARD_IMAGE_CONTAINER.prepend(CARD_IMAGE_ELEMENT);
 
-loadAssignment02 = () => {
-
-    axios.get(BASEURL_02_INITIALIZE_DECK)
-        .then((response) => {
-
-            BASEURL_02_GET_DECK = `https://deckofcardsapi.com/api/deck/${response.data.deck_id}/draw/?count=1`;
-            // console.log(BASEURL_02_GET_DECK);
-            drawNextCard();
-
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    }
 
 }
 
@@ -73,14 +67,14 @@ loadAssignment02 = () => {
 addEventListener('load', (event) => {
 
     fetchFavoriteNumber();  // 01
-    loadAssignment02();     // 02
+    assignment02.init();    // 02
 
-});
+    document.getElementById('button_get02').addEventListener('click', assignment02.drawNextCard);
 
-document.getElementById('button_get02').addEventListener('click', drawNextCard);
+    document.getElementById('button_reset02').addEventListener('click', () => {
 
-document.getElementById('button_reset02').addEventListener('click', () => {
-
-    loadAssignment02();
+        assignment02.init();
     
+    });
+
 });
