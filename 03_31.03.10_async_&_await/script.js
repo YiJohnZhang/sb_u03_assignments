4e2f10363f56a8eaa7777644e4bfb94f75b5ed39
YiJohnZhang <yijohnzhang@gmail.com>
2022-09-11 10:33:32 -0700

    03_31.03.10: finished
    no organized wrapping in an object.

--- 03_31.03.10_async_&_await/script.js
+++ 03_31.03.10_async_&_await/script.js
@@ -23,32 +23,32 @@ const fetchFavoriteNumber = async () => {
 // https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
 // https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=1
 
-const assignment02 = {
+/*const assignment02 = {
 
     async init(){
         // (Re-)Initialize the Deck
 
         const INITIALIZE_DECK_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
         const initialDeckResponse = await axios.get(INITIALIZE_DECK_URL);
-        this.deckID = initialDeckResponse.data.deck_id;
-        console.log(this.deckID);
+        this.DECK_ID = initialDeckResponse.data.deck_id;
+        console.log(this.DECK_ID);
 
     },
 
     async drawNextCard(){
 
-        console.log(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=1`)
+        console.log(`https://deckofcardsapi.com/api/deck/${this.DECK_ID}/draw/?count=1`)
 
         try{
-            const drawDeckResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=1`);
-            this.displayDrawInformation(cardDrawn = drawDeckResponse.data.cards[0], cardsRemaining = drawDeckResponse.data.remaining);
+            const drawDeckResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${this.DECK_ID}/draw/?count=1`);
+            this.displayDrawInformation(cardDrawn = drawDeckResponse.data.cards[0], remainingCards = drawDeckResponse.data.remaining);
         }catch(error){
             console.log(error);
         }
 
     }, 
 
-    displayDrawInformation(){
+    displayDrawInformation: function(cardDrawn, remainingCards) {
 
         document.getElementById('02_cardsLeft').innerText = `${remainingCards} Cards Remaining`;
 
@@ -61,19 +61,61 @@ const assignment02 = {
 
     }
 
+}*/
+
+let DECK_ID;
+
+displayDrawInformation = (cardDrawn, remainingCards) => {
+
+    document.getElementById('02_cardsLeft').innerText = `${remainingCards} Cards Remaining`;
+
+    // const CARD_IMAGE
+    const CARD_IMAGE_ELEMENT = document.createElement('img');
+    CARD_IMAGE_ELEMENT.src = cardDrawn.image;
+
+    const CARD_IMAGE_CONTAINER = document.getElementById('02_cardGame');
+    CARD_IMAGE_CONTAINER.prepend(CARD_IMAGE_ELEMENT);
+
 }
+
+const drawNextCard = async () => {
+
+    try{
+
+        const drawDeckResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${DECK_ID}/draw/?count=1`);
+        this.displayDrawInformation(cardDrawn = drawDeckResponse.data.cards[0], remainingCards = drawDeckResponse.data.remaining);
+    
+    }catch(error){
+
+        console.log(error);
+
+    }
+
+}
+
+const initializeNewDeck = async () => {
+    // (Re-)Initialize the Deck
+
+    const INITIALIZE_DECK_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+    const initialDeckResponse = await axios.get(INITIALIZE_DECK_URL);
+    DECK_ID = initialDeckResponse.data.deck_id;
+    // console.log(DECK_ID);
+
+}
+
 
 // General
 addEventListener('load', (event) => {
 
     fetchFavoriteNumber();  // 01
-    assignment02.init();    // 02
+    initializeNewDeck();    // 02
 
-    document.getElementById('button_get02').addEventListener('click', assignment02.drawNextCard);
+    // Button Doesn't work BEFORE DECK_ID is non-null/non-undefined
+    document.getElementById('button_get02').addEventListener('click', drawNextCard);
 
     document.getElementById('button_reset02').addEventListener('click', () => {
 
-        assignment02.init();
+        initializeNewDeck();
     
     });
 
