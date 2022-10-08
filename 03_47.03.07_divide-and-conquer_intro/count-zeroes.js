014b6ed025ba991d42861023d4b3c272ff2b2958
YiJohnZhang <yijohnzhang@gmail.com>
2022-10-08 14:42:26 -0700

    fin `count-zeroes.js`
    kept messing up the updated pointer ._.

--- 03_47.03.07_divide-and-conquer_intro/count-zeroes.js
+++ 03_47.03.07_divide-and-conquer_intro/count-zeroes.js
@@ -1,5 +1,38 @@
-function countZeroes() {
-  
+const returnMedian = (leftIndex, rightIndex) => {
+	return Math.floor((leftIndex+rightIndex)/2);
+}
+
+function countZeroes(testArray) {
+
+	// m, l, r = shifting middleIndex, leftIndex, rightIndex respectively.
+	//binary search.
+	//find the first 0.
+
+	let l = 0;
+	let r = testArray.length-1;
+	let m = returnMedian(l, r);
+
+	let zeroIndex = testArray.length;
+
+	while(l <= r){
+
+		let m = returnMedian(l, r);
+		
+		if(testArray[m] === 1){
+
+			l = m+1;	// look right.
+
+		}else{
+
+			zeroIndex = m;	// zero found
+			r = m-1;
+
+		}
+	
+	}
+
+	return testArray.length - zeroIndex;
+
 }
 
 module.exports = countZeroes
