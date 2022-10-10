3a773a3f8c9ee0c4dc8c0b88b14c3c2cb75c04c7
YiJohnZhang <yijohnzhang@gmail.com>
2022-10-09 21:50:31 -0700

    finished main assignment of 47.04.12
    finished main assignment of 47.04.12

--- 03_47.04.12_array_and_linked_lists/linked-list.js
+++ 03_47.04.12_array_and_linked_lists/linked-list.js
@@ -1,76 +1,252 @@
 /** Node: node for a singly linked list. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** LinkedList: chained together nodes. */
 
 class LinkedList {
-  constructor(vals = []) {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+	constructor(vals = []) {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;	
+		for (let val of vals) this.push(val);
+	}	
 
-    for (let val of vals) this.push(val);
-  }
+	/** push(val): add new value to end of list. */	
+	push(val) {
 
-  /** push(val): add new value to end of list. */
+		const newNode = new Node(val);
 
-  push(val) {
+		if(this.length == 0){		
+			this.head = newNode;
+			this.tail = newNode;
+		}else{
+			this.tail.next = newNode;
+		}
+		
+		this.tail = newNode;
+		this.length += 1;
+		return;
 
-  }
+	}
 
-  /** unshift(val): add new value to start of list. */
+	/** unshift(val): add new value to start of list. */	
+	unshift(val) {
 
-  unshift(val) {
+		const newNode = new Node(val);
+		newNode.next = this.head;
 
-  }
+		if(this.length == 0)
+			this.tail = newNode;
+			// first operation is unshift
 
-  /** pop(): return & remove last item. */
+		if(this.length == 1)
+			this.tail = this.head;	// implies there is 1 node already in the list
+		
+		this.head = newNode;
+		this.length += 1;
+		return;
 
-  pop() {
+	}
 
-  }
+	/** pop(): return & remove last item. */	
+	pop() {
 
-  /** shift(): return & remove first item. */
+		if(this.length == 0)
+			return 'error: empty linked list'
 
-  shift() {
+		const selectedNodeValue = this.tail.val;
 
-  }
+		let currentNode = this.head;
 
-  /** getAt(idx): get val at idx. */
+		while(currentNode){
+			if(currentNode.next === this.tail){
+				
+				currentNode.next = null;
+				this.tail = currentNode;
 
-  getAt(idx) {
+			}
 
-  }
+			currentNode = currentNode.next;
+		}
 
-  /** setAt(idx, val): set val at idx to val */
+		this.length -= 1;
 
-  setAt(idx, val) {
+		if(this.length == 1)
+			this.tail = this.head;
 
-  }
+		if(this.length == 0){
+			this.head = null;
+			this.tail = null;	
+		}
 
-  /** insertAt(idx, val): add node w/val before idx. */
+		return selectedNodeValue;
 
-  insertAt(idx, val) {
+	}
 
-  }
+	/** shift(): return & remove first item. */	
+	shift() {
 
-  /** removeAt(idx): return & remove item at idx, */
+		const selectedNodeValue = this.head.val;
+		this.head = this.head.next;
 
-  removeAt(idx) {
+		this.length -= 1;
 
-  }
+		if(this.length == 0)
+			this.tail = this.head;
+		
+		return selectedNodeValue;
 
-  /** average(): return an average of all values in the list */
+	}
 
-  average() {
-    
-  }
+	/** getAt(idx): get val at idx. */	
+	getAt(idx) {
+
+		if(idx > this.length)
+			return 'error: out of bounds';
+
+		if(idx == this.length)
+			return this.tail.val;
+			// funny, I tested this and `getAt` doesn't seem to shortcut to this one ._.
+
+		let currentIndex = 0;
+		let currentNode = this.head;
+		while(currentIndex <= idx){
+
+			if(currentIndex == idx)
+				return currentNode.val;
+
+			currentNode = currentNode.next;
+			currentIndex += 1;
+		}
+
+		return;
+
+	}
+
+	/** setAt(idx, val): set val at idx to val */	
+	setAt(idx, val) {
+
+		if(idx > this.length)
+			return 'error: out of bounds';
+
+		if(idx == this.length)
+			this.tail.val = val;
+
+		let currentIndex = 0;
+		let currentNode = this.head;
+		while(currentIndex <= idx){
+
+			if(currentIndex == idx)
+				currentNode.val = val;
+
+			currentNode = currentNode.next;
+			currentIndex += 1;
+
+		}
+
+		return;
+
+	}
+
+	/** returns a node object at the current node index */
+	middleLinkedListNodeUpdateHelper = (idx) => {
+
+		let currentNode = this.head;
+		let currentIndex = 0;
+
+		while(currentIndex <= idx){
+
+			if(currentIndex + 1 == idx){
+				
+				return currentNode;
+
+			}
+
+			currentIndex += 1;
+			currentNode = currentNode.next;
+
+		}
+
+	}
+
+	/** insertAt(idx, val): add node w/val before idx. */	
+	insertAt(idx, val) {
+
+		if(idx > this.length)
+			return 'error: out of bounds';
+
+		if(idx == 0){
+			this.unshift(val);
+			return;
+		}
+		
+		if(idx == this.length){
+			this.push(val);
+			return;
+		}
+
+		const newNode = new Node(val);
+
+		const currentNode = this.middleLinkedListNodeUpdateHelper(idx);
+		newNode.next = currentNode.next;
+		currentNode.next = newNode;
+
+		this.length += 1;
+
+		return;
+
+	}
+
+	/** removeAt(idx): return & remove item at idx, */	
+	removeAt(idx) {
+
+		if(idx > this.length)
+			return 'error: out of bounds';
+
+		if(idx == 0){
+			this.shift();
+			return;
+		}
+		
+		if(idx == this.length){
+			this.pop();
+			return;
+		}
+
+		const currentNode = this.middleLinkedListNodeUpdateHelper(idx);
+		const selectedNodeValue = currentNode.next.value;
+		currentNode.next = curentNode.next.next;
+		
+		this.length -= 1;
+
+		return selectedNodeValue;
+
+	}
+
+	/** average(): return an average of all values in the list. Documentation UPDATE to test returns 0 for empty lists. */	
+	average() {
+
+		if(this.length == 0)
+			return 0;
+
+		let currentNode = this.head;
+		let sum = 0;
+		while(currentNode){
+
+			sum += currentNode.val;
+			currentNode = currentNode.next;
+
+		}
+
+		return sum/this.length;
+	
+	}
 }
 
 module.exports = LinkedList;
